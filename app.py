375f8d838ac2712a887cc27941c4cf3a325e025e
Jiaming He <jminghe1@gmail.com>
2021-03-19 17:15:13 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    RF_model = open('rf_grid.pkl','rb')
+    model = joblib.load(RF_model)
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = int(prediction[0])
+    result = {'Survived':output}
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
